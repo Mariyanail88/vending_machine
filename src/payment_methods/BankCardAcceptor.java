7f94f3b06ecca6710dc5460bac8d7769862dba60
Mariyanail88 <mariasherbakiva@mail.ru>
2023-12-08 19:27:27 +0600

    added field bank card to BankCardAcceptor class

--- src/payment_methods/BankCardAcceptor.java
+++ src/payment_methods/BankCardAcceptor.java
@@ -2,11 +2,15 @@ package payment_methods;
 
 import interfaces.MoneyCollector;
 
+import java.math.BigDecimal;
+
 public class BankCardAcceptor implements MoneyCollector {
     private int amount;
+    private BankCard card;
 
     public BankCardAcceptor(int amount) {
         this.amount = amount;
+        card = new BankCard(BigDecimal.valueOf(500));
     }
 
     @Override
